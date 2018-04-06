7b993a08cd74c0688f9305ef5cada96f80da80a4
jack0lantern <j_j_jiang@yahoo.com>
2018-04-06 16:26:18 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,6 @@
+function toggleComic() {
+	var b = $('body')[0];
+	alert(b);
+}
+// document.addEventListener('onload', function() {alert();});
+// No alert upon loading the extension
---
+++ comic.js
@@ -0,0 +1,4 @@
+ // This script does nothing
+function test() {
+	alert();
+}
